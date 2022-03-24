9e910cd465e77a9dbc74fdacb47cf2cb3e1eb755
kyuzu <99307966+kyuzu@users.noreply.github.com>
2022-03-24 11:41:17 +0800

    feat: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div role="presentation" class="scroller">
+            <Nuxt />
+        </div>
+        <V-Pointer />
+        <V-Loader />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DefaultLayout',
+    mounted() {
+        this.logGreeting();
+        this.setViewHeight();
+        window.addEventListener('resize', this.setViewHeight);
+    },
+    methods: {
+        setViewHeight() {
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        },
+        logGreeting() {
+            console.log(
+                "Looking for source code? Why not check the repository itself! Don't forget to leave a ⭐!\n",
+                'Repository: https://github.com/fxrysh/portfolio'
+            );
+        }
+    }
+};
+</script>
+
+<style></style>
