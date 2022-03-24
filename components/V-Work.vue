fa74cdafcd166a8a730d3853c54edc7d21b71f32
kyuzu <99307966+kyuzu@users.noreply.github.com>
2022-03-24 11:51:42 +0800

    feat: `V-Work` component

---
+++ components/V-Work.vue
@@ -0,0 +1,62 @@
+<template>
+    <section
+        ref="works"
+        class="works"
+        aria-label="works section"
+        data-scroll-section
+    >
+        <V-H2 aria-label="work section heading">Work</V-H2>
+        <ul class="works__list" aria-label="works list">
+            <V-Work-Item
+                v-for="(work, key) in works"
+                :key="key"
+                ref="worksListItems"
+                :work="work"
+                :i="key"
+                :aria-label="work.title"
+                class="works__list__item"
+            />
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data: () => ({ works: [] }),
+    async fetch() {
+        this.works = await this.$content('works')
+            .sortBy('createdAt', 'desc')
+            .fetch();
+    }
+};
+</script>
+
+<style lang="scss">
+.works {
+    margin-top: -1px;
+    padding: 4rem clamp(1rem, 7vw, 5rem) 1rem;
+    color: white;
+    background-color: var(--black-color);
+    pointer-events: all;
+
+    &__list {
+        display: grid;
+        justify-items: center;
+        align-items: start;
+        grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+        gap: var(--step-3);
+
+        max-width: 1100px;
+
+        list-style-type: none;
+        margin: 0 auto;
+        padding-inline-start: 0;
+
+        @media screen and(min-width: 798px) {
+            &__item:nth-child(even) {
+                margin-block-start: 35%;
+            }
+        }
+    }
+}
+</style>
